9acda7e0a300cf0908f49bf6d8e2dc3c16e17be2
Juan Manuel Castillo <juancastillo9910@gmail.com>
2021-05-23 14:08:44 -0500

    JWT Token expiracy, user interactions limited, auth module completed

--- back/routes/posts.js
+++ back/routes/posts.js
@@ -1,4 +1,5 @@
 import express from "express";
+import auth from "../middleware/auth.js";
 
 import {
   getPosts,
@@ -12,10 +13,9 @@ import {
 const router = express.Router();
 
 router.get("/", getPosts);
-router.post("/", createPost);
-router.get("/:id", getPost);
-router.patch("/:id", updatePost);
-router.delete("/:id", deletePost);
-router.patch("/:id/likePost", likePost);
+router.post("/", auth, createPost);
+router.patch("/:id", auth, updatePost);
+router.delete("/:id", auth, deletePost);
+router.patch("/:id/likePost", auth, likePost);
 
 export default router;
