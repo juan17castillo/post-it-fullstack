944d06efa1b19f845696333c8477fba2a42c3d96
Juan Manuel Castillo <juancastillo9910@gmail.com>
2021-05-29 21:07:41 -0500

    Controllers functions documented

--- back/controllers/posts.js
+++ back/controllers/posts.js
@@ -5,6 +5,11 @@ import PostMessage from "../models/postMessage.js";
 
 const router = express.Router();
 
+/**
+ * This function is responsible for searching in the Atlas noSQL database, the list of posts published by users
+ * @param  {Request} req It is in charge of handling the incoming request given the page it is on
+ * @param  {Response} res Responsible for responding to the request. 200 OK to return all posts. 500 Failed to make request
+ */
 export const getPosts = async (req, res) => {
   const { page } = req.query;
   try {
@@ -28,6 +33,12 @@ export const getPosts = async (req, res) => {
     res.status(404).json({ message: error.message });
   }
 };
+
+/**
+ * This function is responsible for searching in the Atlas noSQL database, the list of posts created by users given a specific search by post name or tag.
+ * @param  {Request} req It is in charge of handling the incoming request given the page, post name and tag it is on.
+ * @param  {Response} res Responsible for responding to the request. 200 OK to return all filtered posts. 404 In case there are no posts.
+ */
 export const getPostsBySearch = async (req, res) => {
   const { searchQuery, tags } = req.query;
 
@@ -44,6 +55,11 @@ export const getPostsBySearch = async (req, res) => {
   }
 };
 
+/**
+ * This function is responsible for searching in the Atlas noSQL database, a post created by an user given a specific id.
+ * @param  {Request} req It is in charge of handling the incoming request given the post id.
+ * @param  {Response} res Responsible for responding to the request. 200 OK to return all filtered posts. 404 In case the post was not found.
+ */
 export const getPost = async (req, res) => {
   const { id } = req.params;
 
@@ -56,6 +72,11 @@ export const getPost = async (req, res) => {
   }
 };
 
+/**
+ * This function is responsible for creating a post given by a user, in the noSQL Atlas database.
+ * @param  {Request} req It is in charge of attending the incoming request according to a post that it receives.
+ * @param  {Response} res Responsible for responding to the request. 201 Created to return the new post. 409 In case there are conflicts.
+ */
 export const createPost = async (req, res) => {
   const post = req.body;
 
@@ -74,6 +95,11 @@ export const createPost = async (req, res) => {
   }
 };
 
+/**
+ * This function is responsible for updating a post given by a user, in the noSQL Atlas database.
+ * @param  {Request} req It is in charge of attending the incoming request according to a post that it receives.
+ * @param  {Response} res Responsible for responding to the request. 200 OK to return the updated post.
+ */
 export const updatePost = async (req, res) => {
   const { id } = req.params;
   const { title, message, creator, selectedFile, tags } = req.body;
@@ -88,24 +114,34 @@ export const updatePost = async (req, res) => {
   res.json(updatedPost);
 };
 
+/**
+ * This function is responsible for deleting a post given by a user, in the noSQL Atlas database.
+ * @param  {Request} req It is in charge of attending the incoming request according to a post that it receives.
+ * @param  {Response} res Responsible for responding to the request. 200 OK to return the successful message.
+ */
 export const deletePost = async (req, res) => {
   const { id } = req.params;
 
   if (!mongoose.Types.ObjectId.isValid(id))
-    return res.status(404).send(`No post with id: ${id}`);
+    return res.status(404).send(`Ups! No existe un post con id: ${id}`);
 
   await PostMessage.findByIdAndRemove(id);
 
-  res.json({ message: "Post deleted successfully." });
+  res.json({ message: "Post eliminado exitosamente." });
 };
 
+/**
+ * This function is responsible for liking a post given by a user, in the noSQL Atlas database.
+ * @param  {Request} req It is in charge of attending the incoming request according to a post that it receives.
+ * @param  {Response} res Responsible for responding to the request. 200 OK to return the liked post. 409 No Post found with that id.
+ */
 export const likePost = async (req, res) => {
   const { id } = req.params;
 
-  if (!req.userId) return res.json({ message: "Unauthenticated" });
+  if (!req.userId) return res.json({ message: "Sin autenticarse" });
 
   if (!mongoose.Types.ObjectId.isValid(id))
-    return res.status(404).send(`No post with id: ${id}`);
+    return res.status(404).send(`Ups! No existe un post con id: ${id}`);
 
   const post = await PostMessage.findById(id);
 
--- back/controllers/user.js
+++ back/controllers/user.js
@@ -3,13 +3,18 @@ import jwt from "jsonwebtoken";
 
 import User from "../models/user.js";
 
+/**
+ * This function takes care of giving a user login after performing checks on the noSQL Atlas database. A token corresponding to one hour is generated and the password stored in the DB is decrypted.
+ * @param  {Request} req It is in charge of attending the incoming request given an Email and password.
+ * @param  {Response} res Responsible for responding to the request. 200 OK to return a token with an existing user. 500 Something went wrong.
+ */
 export const signin = async (req, res) => {
   const { email, password } = req.body;
 
   try {
     const existingUser = await User.findOne({ email });
     if (!existingUser) {
-      return res.status(404).json({ message: "User does not exist." });
+      return res.status(404).json({ message: "El usuario no existe" });
     }
     const isPasswordCorrect = await bcrypt.compare(
       password,
@@ -17,7 +22,9 @@ export const signin = async (req, res) => {
     );
 
     if (!isPasswordCorrect) {
-      return res.status(400).json({ message: "Invalid credentials." });
+      return res
+        .status(400)
+        .json({ message: "Verifica de nuevo tus credenciales." });
     }
 
     const token = jwt.sign(
@@ -28,21 +35,26 @@ export const signin = async (req, res) => {
 
     res.status(200).json({ result: existingUser, token });
   } catch (error) {
-    res.status(500).json({ message: "Something went wrong" });
+    res.status(500).json({ message: "Ups! Algo no funcionó" });
   }
 };
 
+/**
+ * This function is responsible for registering a user on the platform given a form. It performs the necessary verifications to generate an encrypted password that will be stored in the DB and a token that will be stored in the local Storage while the session lasts.
+ * @param  {Request} req It is in charge of attending the incoming request given an Email, password, confirmed password, first name and last name.
+ * @param  {Response} res Responsible for responding to the request. 201 Created to return a token with an existing user. 400 Bad Credentials. 500 Bad credentials.
+ */
 export const signup = async (req, res) => {
   const { email, password, confirmPassword, firstName, lastName } = req.body;
 
   try {
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      return res.status(400).json({ message: "User already exists." });
+      return res.status(400).json({ message: "El correo ingresado ya existe" });
     }
 
     if (password !== confirmPassword) {
-      return res.status(400).json({ message: "Passwords don't match." });
+      return res.status(400).json({ message: "Las contraseñas no coinciden" });
     }
 
     const hashedPassword = await bcrypt.hash(password, 12);
@@ -50,7 +62,7 @@ export const signup = async (req, res) => {
     const result = await User.create({
       email,
       password: hashedPassword,
-      name: `${firstName} ${lastName}`
+      name: `${firstName} ${lastName}`,
     });
 
     const token = jwt.sign({ email: result.email, id: result._id }, "test", {
@@ -58,6 +70,6 @@ export const signup = async (req, res) => {
     });
     res.status(201).json({ result, token });
   } catch (error) {
-    res.status(500).json({ message: "Something went wrong" });
+    res.status(500).json({ message: "Ups! Algo no funcionó" });
   }
 };
